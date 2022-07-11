b8250e3785131ce3fe227c3cc2f89a6f8be666b4
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2022-07-11 12:45:19 -0700

    standard_form_to_dictionary converts stdin input to a 2d numpy array of the LP in dictionary form

--- lpsolver.py
+++ lpsolver.py
@@ -15,3 +15,14 @@ if (debug):
     print(lp)
 
 
+# Takes in default standard form LP as a list and outputs the same LP as its corresponding dictionary matrix
+def standard_form_to_dictionary(lp):
+    non_basic = [[0] + lp[0]]
+    basics = []
+    for coeffs in lp[1:]:
+        basics.append([coeffs[-1]] + [-1*i for i in coeffs[:-1]])
+    dictionary = numpy.array(non_basic + basics)
+    print(dictionary)
+
+
+standard_form_to_dictionary(lp)
