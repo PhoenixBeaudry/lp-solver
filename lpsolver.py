55128b60ce18ff8d6819b94276b6e547dcacddb3
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2022-07-11 13:38:23 -0700

    Refactored code to use main()

--- lpsolver.py
+++ lpsolver.py
@@ -3,18 +3,6 @@ import sys
 debug = False
 
 
-# Read in LP from stdin
-lp = []
-for line in sys.stdin:
-    coeff_line = []
-    for coeff in line.split():
-        coeff_line.append(float(coeff))
-    lp.append(coeff_line)
-
-if (debug):
-    print(lp)
-
-
 # Takes in default standard form LP as a list and outputs the same LP as its corresponding dictionary matrix
 def standard_form_to_dictionary(lp):
     non_basic = [[0] + lp[0]]
@@ -22,7 +10,28 @@ def standard_form_to_dictionary(lp):
     for coeffs in lp[1:]:
         basics.append([coeffs[-1]] + [-1*i for i in coeffs[:-1]])
     dictionary = numpy.array(non_basic + basics)
-    print(dictionary)
+    return dictionary
 
 
-standard_form_to_dictionary(lp)
+
+
+
+def main():
+    # Read in LP from stdin
+    lp = []
+    for line in sys.stdin:
+        coeff_line = []
+        for coeff in line.split():
+            coeff_line.append(float(coeff))
+        lp.append(coeff_line)
+
+    # Convert LP into dictionary matrix form
+    dictionary = standard_form_to_dictionary(lp)
+
+    if (debug):
+        print(dictionary)
+
+
+if __name__ == "__main__":
+    main()
+
